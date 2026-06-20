9082711a684a067aa31d2464ef18592036fae00f
johnnashempic-stack <johnnashempic@gmail.com>
2026-06-20 23:22:37 +0800

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('push', function(event) {
-  const data = event.data ? event.data.json() : {title: "R6TBSARI MDT", body: "New Call"};
-  event.waitUntil(
-    self.registration.showNotification(data.title, { body: data.body })
-  );
-});
