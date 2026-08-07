b27ef078f453004024c82ac547568f38e908537e
johnnashempic-stack <johnnashempic@gmail.com>
2026-08-08 07:04:24 +0800

    Add service worker for caching and notifications

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'rsix-tigers-v1';
+const urlsToCache = ['./', './index.html', './app.js', './manifest.json'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(caches.match(event.request).then(r => r || fetch(event.request)));
+});
+
+self.addEventListener('notificationclick', event => {
+  event.notification.close();
+  event.waitUntil(clients.openWindow('./index.html'));
+});
